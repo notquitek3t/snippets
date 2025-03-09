63f627d41a836228fe737fa4aaaa6fe87571c314
kaii :3 (notquitek3t) <72680058+notquitek3t@users.noreply.github.com>
2025-03-08 19:16:00 -0700

    floorpfix

--- windows/scripts/floorp.ps1
+++ windows/scripts/floorp.ps1
@@ -1,4 +1,5 @@
 # firefox one liner install, irm https://snippets.k3t.xyz/windows/scripts/floorp.ps1
 echo "Downloading Floorp..."
 Start-BitsTransfer -Source "https://github.com/Floorp-Projects/Floorp/releases/latest/download/floorp-win64.installer.exe" -Destination $env:TEMP\floorp.exe
-Start-Process $env:TEMP\floorp.exe
+echo "Installing Floorp..."
+Start-Process $env:TEMP\floorp.exe /silent
